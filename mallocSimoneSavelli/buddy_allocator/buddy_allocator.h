4f01b4a96772e640589f6c1abc2e49063851a715
ssimo2301 <116950265+ssimo2301@users.noreply.github.com>
2024-04-21 18:07:53 +0200

    modifiche file h buddy

--- mallocSimoneSavelli/buddy_allocator/buddy_allocator.h
+++ mallocSimoneSavelli/buddy_allocator/buddy_allocator.h
@@ -3,37 +3,21 @@
 
 //blocco del buddy
 typedef struct BuddyListItem{
-  ListItem list;
-  int index;  //indice dell'albero
-  int level;  // livello del buddy
-  char* start;  //inizio della zona di memoria
   int size;
-  struct BuddyListItem* buddy_ptr;
-  struct BuddyListItem* parent_ptr;
-} BuddyListItem;
+  int is_free;
+  struct Block *next;
+} Buddy;
 
-
-//allocatore del buddy
-typedef struct {
-  ListHead free[MAX_LEVELS];
-  ListHead occupied[MAX_LEVELS];
-  int num_levels;
-  PoolAllocator list_allocator;  //allocatore degli elementi della lista
-  int min_bucket_size;
-} BuddyAllocator;
-
+//inizializza il buddy
+void buddyAllocator_init(int size);
 
 //allocatore di memoria
-void* BuddyAllocator_malloc(*BuddyAllocator* alloc, int size);
-
- //restituisce il buddy
-BuddyListItem* BuddyAllocator_getBuddy(BuddyAllocator* alloc, int level);
-
-//rilascia il buddy
-void BuddyAllocator_releaseBuddy(BuddyAllocator* alloc, BuddyListItem* item);
+void* buddyAllocator_malloc(int size);
 
 //free del buddy
-void BuddyAllocator_free(BuddyAllocator* alloc, void* mem);
+void buddyAllocator_free(void* mem);
 
-//inizializzazione del buddy
-void BuddyAllocator_init(BuddyAllocator* alloc, int num_levels, char* buffer, int buffer_size, char* memory, int min_bucket_size);
+//stampa lo stato del buddy
+void buddyAllocator_print();
+
+
