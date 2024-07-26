6a4a3ebff24ae56f282cbeabc287650a08c052fc
ssimo2301 <116950265+ssimo2301@users.noreply.github.com>
2024-07-26 16:25:45 +0200

    Update buddy_allocator.c

--- mallocSimoneSavelli/buddy_allocator/buddy_allocator.c
+++ mallocSimoneSavelli/buddy_allocator/buddy_allocator.c
@@ -4,6 +4,10 @@
 #include "buddy_allocator.h"
 
 #define MIN_BLOCK_SIZE 16
+#define BUFFER_SIZE 102400
+#define BUDDY_LEVELS 9
+#define MEMORY_SIZE (1024*1024)
+#define MIN_BUCKET_SIZE (MEMORY_SIZE>>(BUDDY_LEVELS))
 
 static Buddy *free_list[32]; //array di liste libere
 static int total_memory_size;
@@ -128,3 +132,23 @@ void buddyAllocator_print(){
 		}
 	}
 }
+
+//main 
+char buffer[BUFFER_SIZE];
+char memory[MEMORY_SIZE];
+
+Buddy alloc;
+int main(){
+  int req_size = BuddyAllocator_calcSize(BUDDY_LEVELS);
+  printf("size requested for inizialization: %d\n", req_size);
+  printf("init...\n");
+  BuddyAllocator_init(&alloc, BUDDY_LEVELS, buffer, BUFFER_SIZE, memory, MIN_BUCKET_SIZE);
+  printf("done\n");
+  void* p1 = BuddyAllocator_malloc(100);
+  void* p2 = BuddyAllocator_malloc(100);
+  void* p3 = BuddyAllocator_malloc(100000);
+  BuddyAllocator_free(p1);
+  BuddyAllocator_free(p2);
+  BuddyAllocator_free(p3);
+}
+
