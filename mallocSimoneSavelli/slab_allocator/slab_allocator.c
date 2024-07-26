4d875bb81e8da3ce5e8c4702815caa125b0840c2
ssimo2301 <116950265+ssimo2301@users.noreply.github.com>
2024-07-26 16:31:52 +0200

    Update slab_allocator.c

--- mallocSimoneSavelli/slab_allocator/slab_allocator.c
+++ mallocSimoneSavelli/slab_allocator/slab_allocator.c
@@ -79,3 +79,19 @@ void slabAllocator_deallocateObject(SlabAllocator* allocator, Object *obj){
 }
 
 
+//main
+int main(){
+	SlabAllocator *allocator = slab_allocator_create();
+
+	Object *o1 = slabAllocator_object(allocator);
+	o1->data = 42;
+
+	Object *o2 = slabAllocator_object(allocator);
+	o2->data = 99;
+
+	slabAllocator_deallocateObject(allocator, o2);
+	slabAllocator_destroy(allocator);
+
+	return 0; 
+}
+      
